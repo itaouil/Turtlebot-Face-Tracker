9d144570266d5d3e9229e7cbda39fa25c356f196
itaouil <itaouil95@gmail.com>
2017-10-19 16:49:18 +0100

    Added config loading and ROS to OpenCV image conversion.

--- face_detector/src/face_detector.py
+++ face_detector/src/face_detector.py
@@ -3,8 +3,8 @@
 import rospy
 import cv2
 
-from geometry_msgs.msg import Twist, Vector3
 from sensor_msgs.msg import Image
+from geometry_msgs.msg import Twist, Vector3
 from cv_bridge import CvBridge, CvBridgeError
 
 """
@@ -19,8 +19,50 @@ class Face_Detector:
     # Constructor
     def __init__(self):
 
-        # OpenCV bridge
-        self.cv_bridge = CvBridge()
+        # ROS/OpenCV bridge
+        self.bridge = CvBridge()
 
-        # Camera image subscription
-        self.image_sub = rospy.Subscriber("/camera/rgb/image_raw", Image, self.process)
+        # Image transportation
+        self.image_sub = rospy.Subscriber("/camera/rgb/image_raw", Image, self.callback)
+        self.image_pub = rospy.Publisher("/face_centroid")
+
+        # Config variables
+        screenmaxx              = ""
+        center_offset           = ""
+        face_tracking           = ""
+        haar_file_face          = ""
+        input_image_topic       = ""
+        output_image_topic      = ""
+        display_original_image  = ""
+        display_tracking_image  = ""
+
+        # Load config file
+        try:
+            rospy.get_param("input_image_topic", input_image_topic)
+            rospy.get_param("output_image_topic", output_image_topic)
+            rospy.get_param("haar_file_face", haar_file_face)
+            rospy.get_param("face_tracking", face_tracking)
+            rospy.get_param("display_original_image", display_original_image)
+            rospy.get_param("display_tracking_image", display_tracking_image)
+            rospy.get_param("center_offset", center_offset)
+            rospy.get_param("screenmaxx", screenmaxx)
+
+            rospy.loginfo("Configuration file loaded !")
+
+        except Exception as e:
+            rospy.logwarn("Error during config loading: %s", e)
+
+    # Callback (process image and detects face)
+    def callback(self, image_raw):
+
+        # Convert image to OpenCV MAT type
+        try:
+            cv_image = self.bridge.imgmsg_to_cv2(image_raw, "bgr8")
+
+        except CvBridgeError as e:
+            rospy.logwarn("Error occured during image conversion: %s", e)
+
+        # Show image
+        cv2.namedWindow("Original Image")
+        cv2.imshow("Original Image", cv_image)
+        cv2.waitKey(5)
